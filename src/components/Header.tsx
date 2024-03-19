8d4e02807fc4827abf6a6038e0fe9f7545f81efe
jisu <ahteostnqls@naver.com>
2024-03-19 21:41:36 +0900

    [MOD] header 컴포넌트 - 로고 영역 ui 작업

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import Link from 'next/link';
+import React from 'react';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex p-4 justify-between">
+      <h1>
+        <Link href="/">똥간은 어디에?</Link>
+      </h1>
+    </header>
+  );
 };
 
 export default Header;
