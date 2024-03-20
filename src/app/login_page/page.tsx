fe6cc8d218b261e159904bbc75a45591b51aedfc
ketchup0211 <dlcka1111@naver.com>
2024-03-20 11:15:21 +0900

    [MOD] : login 및 signup page 분리 및 초기 UI 완성

---
+++ src/app/login_page/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-left">
+        <h1 className="text-4xl font-semibold ">로그인</h1>
+
+        <form className="flex flex-col pt-10">
+          <label htmlFor="id" className="">
+            아이디
+          </label>
+          <input id="id" type="text" placeholder="ID" />
+          <br />
+          <label htmlFor="password">비밀번호</label>
+          <input id="id" type="password" placeholder="PASSWORD" />
+          <br />
+          <button>로그인하기</button>
+        </form>
+
+        <div className="pt-5">
+          <p>
+            아직 회원이 아니신가요?
+            <span className="cursor-pointer">회원가입</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/app/signup_page/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+
+const Signup = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <div className="text-left">
+        <h1 className="text-4xl font-semibold ">회원가입</h1>
+
+        <form className="flex flex-col pt-10">
+          <label htmlFor="id" className="">
+            아이디
+          </label>
+          <input id="id" type="text" placeholder="ID" />
+          <br />
+          <label htmlFor="password">비밀번호</label>
+          <input id="id" type="password" placeholder="PASSWORD" />
+          <br />
+          <button>회원가입하기</button>
+        </form>
+
+        <div className="pt-5">
+          <p>
+            이미 회원이신가요?
+            <span className="cursor-pointer">로그인</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
