235eb55f7ae6012bf60837d990c9413a61a0c5bf
jisu <ahteostnqls@naver.com>
2024-03-20 12:14:53 +0900

    [ADD] 현재 좌표 가져오는 기능 작업

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,23 @@
+'use client';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import logoImage from '../assets/images/logo.png';
 
 const Header = () => {
+  //현재 좌표 가져오기
+  const handleGetCurrentPosition = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log('현재 좌표 : ', position.coords.latitude, position.coords.longitude);
+    });
+  };
+
   return (
     <header className="flex p-5 justify-between items-center">
-      <button type="button">현재 나의 위치</button>
+      <button type="button" onClick={handleGetCurrentPosition}>
+        현재 나의 위치
+      </button>
       <h1>
         <Link href="/">
           <Image src={logoImage} alt="똥간 어디에?" width={224} height={34} />
