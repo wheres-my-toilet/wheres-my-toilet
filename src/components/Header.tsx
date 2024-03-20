60752e4592262cd119002c9dd6ec9f7a2e04c1df
jisu <ahteostnqls@naver.com>
2024-03-20 10:35:00 +0900

    [MOD] Header 컴포넌트 로고 추가 및 ui 작업

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,18 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import logoImage from '../assets/images/logo.png';
 
 const Header = () => {
   return (
-    <header className="flex p-4 justify-between">
+    <header className="flex p-5 justify-between items-center">
+      <button type="button">현재 나의 위치</button>
       <h1>
-        <Link href="/">똥간은 어디에?</Link>
+        <Link href="/">
+          <Image src={logoImage} alt="똥간 어디에?" width={224} height={34} />
+        </Link>
       </h1>
+      <Link href="/loginNsignup_page">로그인</Link>
     </header>
   );
 };
