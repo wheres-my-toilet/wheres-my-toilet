e1d28f28db35939863e65dc4ec85c293b1f409b9
ketchup0211 <dlcka1111@naver.com>
2024-03-20 17:53:01 +0900

    [ADD] Login logic 구현 70% 완료 : 유저 정보 전달 및 에러 핸들링 제외

---
+++ src/components/login_page/Form.tsx
@@ -0,0 +1,73 @@
+import { supabase } from '@/shared/supabase/supabase';
+import { useRouter } from 'next/navigation';
+import { ChangeEvent, FormEvent, useState } from 'react';
+
+export function Form() {
+  const router = useRouter();
+  const [loginInfo, setLoginInfo] = useState({ email: '', password: '' });
+  //TODO: 에러 유형 추가
+
+  const handleLoginInfo = {
+    handleEmail: (event: ChangeEvent<HTMLInputElement>) => {
+      setLoginInfo((prev) => ({
+        ...prev,
+        email: event.target.value,
+      }));
+    },
+    handlePassword: (event: ChangeEvent<HTMLInputElement>) => {
+      setLoginInfo((prev) => ({
+        ...prev,
+        password: event.target.value,
+      }));
+    },
+  };
+
+  const handleLogin = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: loginInfo.email,
+        password: loginInfo.password,
+      });
+      if (error) {
+        switch (error.message) {
+          //TODO : error handling logic 추가
+          default:
+            alert(`에러가 발생하였습니다.\n${error.message}`);
+        }
+        return;
+      }
+      if (data) {
+        //TODO : 유저 정보를 넘겨주는 로직(zustand 상태관리 필요)
+        router.push('/home_page');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="flex flex-col pt-10" onSubmit={handleLogin}>
+      <label htmlFor="email">이메일</label>
+      <input
+        id="email"
+        type="text"
+        placeholder="EMAIL"
+        value={loginInfo.email}
+        onChange={handleLoginInfo.handleEmail}
+        autoComplete="off"
+      />
+      <br />
+      <label htmlFor="password">비밀번호</label>
+      <input
+        id="password"
+        type="password"
+        placeholder="PASSWORD (6+)"
+        value={loginInfo.password}
+        onChange={handleLoginInfo.handlePassword}
+      />
+      <br />
+      <button className="bg-gray-400">로그인하기</button>
+    </form>
+  );
+}
