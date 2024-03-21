ac5c51ed7f43e9dee93ccd02080bfb41dff9e473
jisu <ahteostnqls@naver.com>
2024-03-21 23:20:27 +0900

    [MOD] bookmark list 컴포넌트 ui 작업

--- src/components/bookmark_page/BookmarkList.tsx
+++ src/components/bookmark_page/BookmarkList.tsx
@@ -2,6 +2,7 @@
 
 import { supabase } from '@/shared/supabase/supabase';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import Link from 'next/link';
 import React from 'react';
 
 type Bookmark = {
@@ -74,15 +75,21 @@ const BookmarkList = () => {
   return (
     <>
       {data && data.length ? (
-        <ul className="grid grid-cols-4 sm:grid-cols-1 md:grid-cols-2 gap-4">
+        <ul className="grid grid-cols-1 md:grid-cols-2 gap-4 m-4">
           {data?.map((item) => {
             return (
-              <li key={item.bookmark_id}>
-                <strong>{item.toilet_location.toilet_name}</strong>
-                <p>{item.toilet_location.toilet_address}</p>
-                <button type="button" onClick={() => handleCancelBookmark(item.bookmark_id)}>
-                  ★ 취소
-                </button>
+              <li key={item.bookmark_id} className="p-4 border rounded-md min-h-28 xl:min-h-40">
+                <div className="flex justify-between items-start">
+                  <strong className="text-base md:text-xl">{item.toilet_location.toilet_name}</strong>
+                  <button
+                    type="button"
+                    className="min-w-14 text-gray-400"
+                    onClick={() => handleCancelBookmark(item.bookmark_id)}
+                  >
+                    <span className="text-amber-300">★</span> 취소
+                  </button>
+                </div>
+                <p className="text-neutral-600">{item.toilet_location.toilet_address}</p>
               </li>
             );
           })}
