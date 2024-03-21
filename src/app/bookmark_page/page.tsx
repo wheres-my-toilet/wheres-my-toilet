a0ab781b7081afb59c11c9966c98d99130692e1e
jisu <ahteostnqls@naver.com>
2024-03-21 23:16:25 +0900

    [MOD] 페이지 타이틀 추가

--- src/app/bookmark_page/page.tsx
+++ src/app/bookmark_page/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const BookmarkPage = () => {
   return (
     <div className="container mx-auto">
+      <h2 className="text-xl md:text-2xl font-bold text-center my-6">즐겨찾는 화장실</h2>
       <BookmarkList />
     </div>
   );
