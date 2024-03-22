c95961d8a353615a1b02150f9d82e054ea81164d
HuaHuaChiChi <ksksks102@naver.com>
2024-03-22 19:43:41 +0900

    [MOD] : ui 수정

--- src/app/tip_page/page.tsx
+++ src/app/tip_page/page.tsx
@@ -5,9 +5,8 @@ const page = () => {
     <div>
       <div className="h-72 border-t-2 border-black border-b-2 flex items-center justify-center">
         <div>
-          <h1 className="text-xl mb-4">오늘의 TIP</h1>
-          <h1 className="text-4xl mb-3"> 제목 1</h1>
-          <h2 className="mb-6">내용 1</h2>
+          <h1 className="text-4xl mb-4">응아 잘 하는 TIP</h1>
+          <h1 className="text-2xl mb-3">관심가는 카드를 클릭해보세요!</h1>
         </div>
       </div>
       <TipList></TipList>
