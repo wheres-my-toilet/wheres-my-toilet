a79807d119ba2be6d7ab097363d357440314765b
jisu <ahteostnqls@naver.com>
2024-03-22 19:58:49 +0900

    [ADD] 북마크한 리뷰 데이터 조회 작업

--- src/util/bookmark_page/api.ts
+++ src/util/bookmark_page/api.ts
@@ -10,6 +10,14 @@ export type Bookmark = {
   user_id: string;
 };
 
+export type ReviewRate = {
+  toilet_id: number;
+  toilet_loc_rate: number;
+  toilet_clean_rate: number;
+  toilet_pop_rate: number;
+};
+
+//유저의 북마크 목록 조회
 export const getData = async (user_id: string): Promise<Bookmark[]> => {
   try {
     const { data, error } = await supabase
@@ -21,8 +29,28 @@ export const getData = async (user_id: string): Promise<Bookmark[]> => {
       console.error('Error fetching data:', error.message);
       return [];
     }
-
+    console.log(data);
     return data as Bookmark[];
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const getReviewData = async (toiletIds: number[]): Promise<ReviewRate[]> => {
+  try {
+    const { data, error } = await supabase
+      .from('review_info')
+      .select('toilet_id, toilet_loc_rate, toilet_clean_rate, toilet_pop_rate')
+      .in('toilet_id', toiletIds); //북마크 된 화장실 id
+    console.log('리뷰조회', data);
+
+    if (error) {
+      console.error('Error fetching data:', error.message);
+      return [];
+    }
+
+    return data as ReviewRate[];
   } catch (error) {
     console.error(error);
     return [];
