52671be0222c9e50036cd659b7ae7e49ad2e08be
ketchup0211 <dlcka1111@naver.com>
2024-03-22 12:46:49 +0900

    [MOD] Intro Page UI 확인

--- src/app/intro_page/page.tsx
+++ src/app/intro_page/page.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+'use client';
 
-const page = () => {
+import React from 'react';
+import Image from 'next/image';
+
+import Icon from '@/assets/images/wheres_my_toilet1_icon.png';
+
+const Intro = () => {
   return (
-    <div className="flex flex-col justify-center">
-      <h1>어디에?</h1>
-      <p>image</p>
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="flex flex-col justify-center items-center gap-10 bg-black">
+        <h1 className="text-6xl tracking-widest text-white">
+          <span className="">똥간</span>은<br />
+        </h1>
+        <h1 className="text-6xl tracking-widest text-white">어디에?</h1>
+        <Image src={Icon} alt="똥간 어디에?" width={224} height={34} />
+      </div>
     </div>
   );
 };
 
-export default page;
+export default Intro;
