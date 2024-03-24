119c056d99ceca4b8034aafac4ba38c780d22b1c
jisu <ahteostnqls@naver.com>
2024-03-24 20:11:52 +0900

    [MOD] 즐겨찾는 화장실 url 변경

--- src/components/home_page/HomeCategory.tsx
+++ src/components/home_page/HomeCategory.tsx
@@ -5,7 +5,7 @@ function HomeCategory() {
     <nav>
       <ul className="border-2  w-100 ml-10 mt-10 p-10 sticky border-black  rounded ">
         <li className="border-2 m-2 p-5 w-200  border-black  font-bold rounded">
-          <Link href={`/login_page`}>즐겨찾기</Link>
+          <Link href={`/bookmark_page`}>즐겨찾기</Link>
         </li>
         <li className="border-2 m-2 p-5 w-40  border-black font-bold  rounded">
           <Link href={`/etiquette_page`}>화장실 에티켓</Link>
