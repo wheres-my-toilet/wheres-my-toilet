0ff1f5418dd8d6bbb340944a7342b8379a0dad51
andan <andante1104@naver.com>
2024-03-25 02:33:03 +0900

    [DESIGN] : home_page 전반적인 디자인 다시함

--- src/components/home_page/HomeCategory.tsx
+++ src/components/home_page/HomeCategory.tsx
@@ -1,20 +1,45 @@
+import { useState } from 'react';
+import { BiCategory } from 'react-icons/bi';
+import { IoIosMan } from 'react-icons/io';
+import { MdFavorite, MdTipsAndUpdates } from 'react-icons/md';
 import Link from 'next/link';
 
 function HomeCategory() {
+  const [isOpen, setIsOpen] = useState(true); // 토글 상태를 관리하기 위한 상태
+
+  const toggleCategory = () => {
+    setIsOpen(!isOpen); // 토글 상태 변경
+  };
+
   return (
-  <nav>
-    <ul className="w-auto m-10">
-      <li className="p-5 border-gray-500 m-3  rounded-md shadow-sm  hover:shadow-md   hover:font-bold hover: hover:shadow-slate-400 ">
-        <Link href={`/bookmark_page`}>Favorite</Link>
-      </li>
-      <li className="p-5 border-gray-500 m-3 rounded-md  shadow-sm hover:shadow-md  hover:font-bold hover:  hover:shadow-slate-400">
-        <Link href={`/etiquette_page`}>Etiquette</Link>
-      </li>
-      <li className="p-5 border-gray-500 m-3  rounded-md  shadow-sm hover:shadow-md   hover:font-bold hover:  hover:shadow-slate-400">
-        <Link href={`/tip_page`}>TIP</Link>
-      </li>
-    </ul>
-  </nav>
+    <nav className="flex justify-left items-start flex-col flex-start md:w-full">
+      <div className="flex items-center cursor-pointer" onClick={toggleCategory}>
+        <BiCategory size={20} className="mr-auto flex justify-start start-0 md:object-no" />
+        <span className="text-gray-800 hover:text-red-500 transition duration-300 p-2">Categories</span>
+      </div>
+      {isOpen && ( // isOpen이 true일 때만 카테고리 목록을 보여줌
+        <ul className="flex flex-col md:flex items-start md:w-full">
+          <li className="hover:border-red-500 rounded-md p-2 flex items-center m-3 border-2 bg-slate-100">
+            <MdFavorite className="text-red-500 mr-2" />
+            <Link href="/bookmark_page" className="text-gray-800 hover:text-red-500 transition duration-300 p-2">
+              Favorite
+            </Link>
+          </li>
+          <li className="hover:border-blue-600 rounded-md p-2 flex items-center m-3 border-2 bg-slate-100">
+            <IoIosMan className="text-blue-600 mr-2" />
+            <Link href="/etiquette_page" className="text-gray-800 hover:text-blue-600 transition duration-300 p-2">
+              Etiquette
+            </Link>
+          </li>
+          <li className="hover:border-yellow-400 rounded-md p-2 flex items-center m-3 border-2 bg-slate-100">
+            <MdTipsAndUpdates className="text-yellow-400 mr-2" />
+            <Link href="/tip_page" className="text-gray-800 hover:text-yellow-400 transition duration-300 p-2">
+              Its TIP
+            </Link>
+          </li>
+        </ul>
+      )}
+    </nav>
   );
 }
 
--- src/components/home_page/HomeSelectForm.tsx
+++ src/components/home_page/HomeSelectForm.tsx
@@ -3,47 +3,52 @@ import { HomeSelectFormType } from '@/types/home_page/types';
 function HomeSelectForm({ selectSee, selectGunGue, handleSelectCity, handleSelectCounty }: HomeSelectFormType) {
   return (
     <>
-      <form>
-        <select
-          value={selectSee}
-          onChange={handleSelectCity}
-          className="border-2 m-2 p-2 text-black border-black font-bold"
-        >
-          <option value="시 선택">시/도 선택</option>
+      <form className=" m-2 flex justify-center align-center ">
+        <div className="flex items-center  justify-between  ">
+          <p className="p-4 font-bold text-1xl  sm:text-xl ">지역명으로 검색하기</p>
+          <div>
+            <select
+              value={selectSee}
+              onChange={handleSelectCity}
+              className="border-2 m-2 p-2 text-black border-black font-bold "
+            >
+              <option value="시 선택">시/도 선택</option>
 
-          <option value="서울특별시">서울특별시</option>
-          <option value="대구광역시">대구광역시</option>
-          <option value="경기도">경기도</option>
-        </select>
+              <option value="서울특별시">서울특별시</option>
+              <option value="대구광역시">대구광역시</option>
+              <option value="경기도">경기도</option>
+            </select>
 
-        <select
-          value={selectGunGue}
-          onChange={handleSelectCounty}
-          className="border-2 m-2 p-2 text-black border-black font-bold"
-        >
-          <option value="구 선택">군/구 선택</option>
+            <select
+              value={selectGunGue}
+              onChange={handleSelectCounty}
+              className="border-2 m-2 p-2 text-black border-black font-bold"
+            >
+              <option value="구 선택">군/구 선택</option>
 
-          {selectSee === '서울특별시' && (
-            <>
-              <optgroup label="서울특별시">
-                <option value="강동구">강동구</option>
-                <option value="양천구">양천구</option>
-              </optgroup>
-            </>
-          )}
+              {selectSee === '서울특별시' && (
+                <>
+                  <optgroup label="서울특별시">
+                    <option value="강동구">강동구</option>
+                    <option value="양천구">양천구</option>
+                  </optgroup>
+                </>
+              )}
 
-          {selectSee === '대구광역시' && (
-            <optgroup label="대구광역시">
-              <option value="군위군">군위군</option>
-            </optgroup>
-          )}
+              {selectSee === '대구광역시' && (
+                <optgroup label="대구광역시">
+                  <option value="군위군">군위군</option>
+                </optgroup>
+              )}
 
-          {selectSee === '경기도' && (
-            <optgroup label="안양시">
-              <option value="석수동">석수동</option>
-            </optgroup>
-          )}
-        </select>
+              {selectSee === '경기도' && (
+                <optgroup label="안양시">
+                  <option value="석수동">석수동</option>
+                </optgroup>
+              )}
+            </select>
+          </div>
+        </div>
       </form>
     </>
   );
